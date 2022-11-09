d79064601cd845bb46a8ffbb2fc951c0d77be64c
Juan Pedro <jtrionfinimed@santandertecnologia.com.ar>
2022-11-09 16:49:01 -0300

    :penguin: new feature Entity CreditCard

--- src/main/java/com/santander/homebanking/models/CreditCard.java
+++ src/main/java/com/santander/homebanking/models/CreditCard.java
@@ -1,5 +1,46 @@
 package com.santander.homebanking.models;
 
-public class CreditCard extends Card{
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.HashSet;
 
+public class CreditCard extends Card{
+    private Long maxLimit = 0L;
+    private Long availableLimit = 0L;
+
+    @OneToMany(mappedBy = "transaction", fetch = FetchType.EAGER)
+    Set<TransactionCreditCard> transactions = new HashSet<>();
+
+    public CreditCard(String cardHolder, String number, Integer cvv, LocalDate fromDate, LocalDate thruDate, CardColor color, CardType type, String pin) {
+        super(cardHolder, number, cvv, fromDate, thruDate, color, type, pin);
+    }
+
+    public CreditCard() {
+
+    }
+
+    public Long getMaxLimit() {
+        return maxLimit;
+    }
+
+    public void setMaxLimit(Long maxLimit) {
+        this.maxLimit = maxLimit;
+    }
+
+    public Long getAvailableLimit() {
+        return availableLimit;
+    }
+
+    public void setAvailableLimit(Long availableLimit) {
+        this.availableLimit = availableLimit;
+    }
+
+    public Set<TransactionCreditCard> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(Set<TransactionCreditCard> transactions) {
+        this.transactions = transactions;
+    }
 }
